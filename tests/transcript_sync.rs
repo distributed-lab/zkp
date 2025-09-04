edf88a212d13fd4e698501efb2f64c9fc7f47f66
Arniiiii <lg3dx6fd@gmail.com>
2025-09-04 15:33:28 +0000

    Prettified test "transcript_sync"

--- tests/transcript_sync.rs
+++ tests/transcript_sync.rs
@@ -9,37 +9,38 @@ use ark_xsk233::affine::Xsk233Affine as G1Affine;
 use ark_xsk233::xsk233::Fr;
 use rand::thread_rng;
 
-use zkp::{toolbox::TranscriptProtocol, BatchableProof, CompactProof, Transcript};
+use zkp::{toolbox::TranscriptProtocol, CompactProof, Transcript};
 
 define_proof! {dleq, "Com(x, r1), Com(x, r2) Proof", (x, r1, r2), (A, B, H), (G) : A = (x * G + r1 * H), B = (x * G + r2 * H) }
 
 #[test]
-fn create_and_verify_compact() {
+fn transcript_sync() {
     let G = G1Affine::generator();
     let H = G1Affine::rand(&mut thread_rng());
+
     // Prover's scope
     let mut transcript1 = Transcript::new(b"DLEQTest");
+
     let x = Fr::from(rand::random::<u64>());
     let r1 = Fr::from(rand::random::<u64>());
     let r2 = Fr::from(rand::random::<u64>());
     let A = (G * x + H * r1).into_affine();
     let B = (G * x + H * r2).into_affine();
-    let (proof, points) = {
-        let (proof, points) = dleq::prove_compact(
-            &mut transcript1,
-            dleq::ProveAssignments {
-                x: &x,
-                r1: &r1,
-                r2: &r2,
-                A: &A,
-                B: &B,
-                G: &G,
-                H: &H,
-            },
-        );
-        (proof, points)
-    };
-    let smth1 =
+
+    let (proof, points) = dleq::prove_compact(
+        &mut transcript1,
+        dleq::ProveAssignments {
+            x: &x,
+            r1: &r1,
+            r2: &r2,
+            A: &A,
+            B: &B,
+            G: &G,
+            H: &H,
+        },
+    );
+
+    let transcript_challenge_bytes_1 =
         <Transcript as TranscriptProtocol<G1Affine>>::get_challenge(&mut transcript1, b"DLEQTest");
 
     let proof_bytes = proof.to_bytes().unwrap();
@@ -58,9 +59,12 @@ fn create_and_verify_compact() {
         },
     )
     .is_ok());
-    let smth2 =
+
+    let transcript_challenge_bytes_2 =
         <Transcript as TranscriptProtocol<G1Affine>>::get_challenge(&mut transcript2, b"DLEQTest");
-    assert_eq!(smth1, smth2);
+
+    assert_eq!(transcript_challenge_bytes_1, transcript_challenge_bytes_2);
+
     let (proof, points) = dleq::prove_compact(
         &mut transcript2,
         dleq::ProveAssignments {
@@ -73,8 +77,10 @@ fn create_and_verify_compact() {
             H: &H,
         },
     );
+
     let proof_bytes = proof.to_bytes().unwrap();
     let parsed_proof: CompactProof<_> = CompactProof::from_bytes(&proof_bytes).unwrap();
+
     assert!(dleq::verify_compact(
         &parsed_proof,
         &mut transcript1,
@@ -86,11 +92,14 @@ fn create_and_verify_compact() {
         },
     )
     .is_ok());
-    let smth3 =
+
+    let transcript_challenge_bytes_3 =
         <Transcript as TranscriptProtocol<G1Affine>>::get_challenge(&mut transcript1, b"DLEQTest");
-    let smth4 =
+
+    let transcript_challenge_bytes_4 =
         <Transcript as TranscriptProtocol<G1Affine>>::get_challenge(&mut transcript2, b"DLEQTest");
-    assert_eq!(smth3, smth4);
-    assert_ne!(smth1, smth3);
-    assert_ne!(smth2, smth4);
+
+    assert_eq!(transcript_challenge_bytes_3, transcript_challenge_bytes_4);
+    assert_ne!(transcript_challenge_bytes_1, transcript_challenge_bytes_3);
+    assert_ne!(transcript_challenge_bytes_2, transcript_challenge_bytes_4);
 }
